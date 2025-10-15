d552dfc65858e1cda51b86db235207b069397327
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2025-10-15 09:50:40 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region                      = "us-east-1" # Or any region - must be a valid region.
+  region                      = "us-west-1" # Or any region - must be a valid region.
   access_key                  = "test"
   secret_key                  = "test"
   skip_credentials_validation = true
-  skip_requesting_account_id  = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
   s3_use_path_style           = true
 
   endpoints {
     apigateway       = "http://localhost:4566"
     cloudformation   = "http://localhost:4566"
-
+    cloudwatch       = "http://localhost:4566"
     dynamodb         = "http://localhost:4566"
     ec2              = "http://localhost:4566"
     es               = "http://localhost:4566"
@@ -31,7 +31,5 @@ provider "aws" {
     ssm              = "http://localhost:4566"
     stepfunctions    = "http://localhost:4566"
     sts              = "http://localhost:4566"
-    cloudwatch = "http://localhost:4566"
-  
   }
 }
